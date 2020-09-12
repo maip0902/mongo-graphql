f32c36e2b8bd466aa79ecd3804db8936be19eed6
神田舞 <maiptomato@gmail.com>
2020-09-12 17:45:31 +0900

    ディレクトリ構成を修正

---
+++ graph/resolver.go
@@ -0,0 +1,22 @@
+package graph
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+import (
+    "github.com/globalsign/mgo"
+    "github.com/maip0902/mongo-graphql/users/graph/generated"
+    "github.com/maip0902/mongo-graphql/db"
+)
+
+type Resolver struct{
+    users *mgo.Collection
+}
+
+func New() *generated.Config {
+    return &generated.Config {
+        Resolvers: &Resolver{
+            users: db.GetCollection("users"),
+        },
+    }
+}
